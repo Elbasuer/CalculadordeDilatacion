6fcc947dadd9d337051dc1da801f66b78f1f8501
Jorge Armando Ramirez Rocha <elbasuerblogger@gmail.com>
2018-04-15 18:34:32 -0600

    lol

--- app/src/main/java/com/cetis87/jorgecarlos/calculadordedilatacion/DilatacionLineal.java
+++ app/src/main/java/com/cetis87/jorgecarlos/calculadordedilatacion/DilatacionLineal.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 public class DilatacionLineal extends AppCompatActivity {
 
 
-    private Button coeficiente, lo, lf, to, tf;
+    private Button coeficiente, lo, lf, to, tf,lol;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dilatacion_lineal);
